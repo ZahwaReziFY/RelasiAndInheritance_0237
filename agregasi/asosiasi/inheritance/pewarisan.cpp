b897aabfdf2b6a9797a516698bef5484dd219f6e
ZahwaReziFY <zahwarezi416@gmail.com>
2025-05-26 20:36:54 +0700

    add member string pNama pSekolah

--- agregasi/asosiasi/inheritance/pewarisan.cpp
+++ agregasi/asosiasi/inheritance/pewarisan.cpp
@@ -23,4 +23,7 @@ class pelajar : public orang {
 public:
     string sekolah;
 
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
     
